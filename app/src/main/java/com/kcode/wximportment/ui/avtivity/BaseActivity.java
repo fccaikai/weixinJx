eb5a3bd7ae0bf814058ff8efaf34abdeda3855a9
fccaikai <714564390@qq.com>
2016-05-13 16:05:45 +0800

    bate版

---
+++ app/src/main/java/com/kcode/wximportment/ui/avtivity/BaseActivity.java
@@ -0,0 +1,7 @@
+package com.kcode.wximportment.ui.avtivity;
+
+/**
+ * Created by caik on 16/5/13.
+ */
+public class BaseActivity {
+}
---
+++ app/src/main/java/com/kcode/wximportment/utils/L.java
@@ -0,0 +1,7 @@
+package com.kcode.wximportment.utils;
+
+/**
+ * Created by caik on 16/5/13.
+ */
+public class L {
+}
